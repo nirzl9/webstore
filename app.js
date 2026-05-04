0a9d1e42ce82be40ce7ee14bfee219d208eb3b14
Nirazan Thapa <nirazanthapa@Nirazans-MacBook-Pro.local>
2026-05-04 15:04:48 +0545

    update

--- app.js
+++ app.js
@@ -4,16 +4,19 @@ import userRouter from "./routes/user.routes.js";
 import inventoryRouter from "./routes/inventory.routes.js";
 import profileRouter from "./routes/profile.routes.js";
 import ordersRouter from "./routes/orders.routes.js";
+import selleruserRouter from "./routes/selleruser.routes.js";
 import connectDB from "./database/mongodb.js";
 
 const app = express();
 app.use(express.json());
 app.use(userRouter);
+app.use(selleruserRouter);
 app.use(ordersRouter);
 app.use(inventoryRouter);
 app.use(profileRouter);
 
 app.use("/api/v1/users", userRouter);
+app.use("/api/v1/sellerusers", selleruserRouter);
 app.use("/api/v1/inventory", inventoryRouter);
 app.use("/api/v1/profile", profileRouter);
 app.use("/api/v1/orders", ordersRouter);
